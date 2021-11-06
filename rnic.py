31d7d7852fa668a4795a0d7165bc92d0278dbdd6
mingman514 <mingyuchoi514@gmail.com>
2021-11-06 22:29:26 +0900

    first

---
+++ rnic.py
@@ -0,0 +1,179 @@
+from pybash import bash, bash_return
+import sys, time
+import vws_util as vu
+from multiprocessing import Process
+
+
+
+################################
+## Basic Info
+################################
+server_ip = ''
+#base_path = '~/'
+save_path = '~/script/test_result'
+
+TESTNAME = 'X4'
+HOST_NAME = bash_return('hostname').decode('utf-8').strip()
+TEST_TYPE = ''
+TX_DEPTH = 128
+MTU = 4096
+MSG_SIZE = -1
+
+
+################################
+## Global Variables
+################################
+Iam = -1
+SERVER = 0
+CLIENT = 1
+# Wait for TF_procs
+TF_procs = []
+BF_procs = []
+
+
+def name_generator(test_t=TEST_TYPE):
+        return '{}_{}_{}_t{}_m{}_bg{}'.format(
+                TESTNAME, HOST_NAME, test_t, TX_DEPTH, MTU, MSG_SIZE)
+
+def initialize():
+    vu.kill_native_process()
+    print('Terminate perf_test Apps')
+
+
+
+
+#def check_jobs():
+#    vu.async_wait()
+   
+
+#def run_tf(test, option):
+#    vu.perf_test(test, option)
+
+
+def run(test, opt):
+    vu.perf_test(test, opt)
+    #print(test_t, opt)
+
+def supervise(opt):
+    print('Start watching for target process to finish...')
+    elapsed = 0
+    FLAG = 1
+    while FLAG:
+        cmds = bash_return('pgrep -a ib_').decode('utf-8').strip()
+#        print(cmds)
+#        print('find:',opt)
+        if opt not in cmds:
+            FLAG = 0
+            print('Target process finished')
+        else:
+            time.sleep(1)
+            print('Target process working... {}s'.format(elapsed))
+            elapsed += 1
+            
+
+if __name__ == '__main__':
+    """
+    ex. python rnic.py 0 10.0.31.2
+    """
+    Iam = int(sys.argv[1])
+    server_ip = sys.argv[2]
+  
+    test_list = ['sb', 'rb', 'wb']    
+    mtu_list = [512, 1024, 2048, 4096]
+    tx_depth_list = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+    msg_size_list = [256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 1048576, 1073741824]
+
+    """
+    Background Flow & Target Flow
+    Watch Target Flow Until Ends
+    """
+    target_t = 'tput' # 'tput' or 'lat'
+
+    initialize()
+    for test_t in test_list:
+        
+        TEST_TYPE = test_t
+        
+        for mtu in mtu_list:
+            MTU = mtu
+
+
+            for tx_depth in tx_depth_list:
+                TX_DEPTH = tx_depth
+
+
+                for msg_size in msg_size_list:
+                    MSG_SIZE = msg_size
+
+                    ####################
+                    ### Background Flow
+                    ####################
+                    default_opt = '-F -l 1 --run_infinitely -d mlx5_0'
+                    opt = default_opt
+                    opt += ' -m ' + str(MTU)
+                    opt += ' -t ' + str(TX_DEPTH)
+                    opt += ' -s ' + str(MSG_SIZE)
+    
+                    f = name_generator(test_t) + '_bf'
+#                    print('bf name: ', f)
+
+                    if Iam == CLIENT:
+                        opt += ' {}'.format(server_ip)
+                        opt += ' > {}/{}'.format(save_path, f)
+
+                    print('Start Background Flow')
+                    bf_proc = Process(target=run, args=(test_t, opt))
+                    bf_proc.start()
+                    
+                    ###################
+                    ### Target Flow
+                    ###################
+                    print('Wait for Background Flow Warming Up')
+                    time.sleep(15)  # set enough time to measure bw of background
+                    print('Start Target Flow')
+                    if target_t == 'tput':
+                        default_opt = '-F -l 64 -s 16 -d mlx5_0 -n 100000000'
+                    elif target_t == 'lat':
+                        default_opt = '-F -l 1 -s 16 -d mlx5_0 -n 10000000 -t ' + str(TX_DEPTH)
+
+                    opt = default_opt
+                    opt += ' -m ' + str(MTU)
+                    
+                    f = name_generator(test_t) if target_t == 'tput' else name_generator(test_t[0]+'l')
+                    f += '_tf'
+#                    print('tf name: ', f)
+
+                    if Iam == CLIENT:
+                        opt += ' {}'.format(server_ip)
+                        opt += ' > {}/{}'.format(save_path, f)
+
+                    tf_proc = Process(target=run, args=(test_t, opt))
+                    tf_proc.start()
+                    
+                    ###################
+                    ### Supervisor
+                    ###################
+                    time.sleep(5)
+                    opt = opt.split('>')[0].strip()
+                    sv_proc = Process(target=supervise, args=(opt,))
+                    sv_proc.start()
+
+                    sv_proc.join()
+
+                    # Sync & Init
+                    initialize()
+                    vu.synchronize(Iam, server_ip)
+                    
+
+
+    # Start Target Flows
+
+#    proc = Process(target=run, args=(Iam))
+#    procs.append(proc)
+#    proc.start()
+#    print(proc.pid)
+
+   
+
+#    time.sleep()
+#    vu.kill_native_process()
---
+++ vws_util.py
@@ -0,0 +1,143 @@
+from pybash import bash, bash_return
+import time
+import socket
+
+PASSWD = 'aldrn5'
+
+############################################
+# Basic
+############################################
+def get_self_ip():
+    ip_list = bash_return('hostname -I').decode('utf-8')
+    ip = ip_list.split(' ')
+
+    ip10 = ''
+    for _ip in ip:
+        if _ip[0:3] == '10.':
+            ip10 = _ip
+            break
+
+    return ip10
+
+def build_using_sh(cont, filepath):
+    # Usage
+    # build_using_sh('vws_node1', '/freeflow/vws_freeflow/libvws/build.sh')
+    path = '/'.join(filepath.split('/')[:-1])
+    filename = filepath.split('/')[-1]
+    bash('docker exec {} sh -c "cd {}; ./{}"'.format(cont, path, filename))
+
+
+def mkdir(path):
+    bash('mkdir {}'.format(path))
+
+def test_t_translate(test):
+    testname = ''
+    if test == 'sb':
+        testname = 'ib_send_bw'
+    elif test == 'sl':
+        testname = 'ib_send_lat'
+    elif test == 'wb':
+        testname = 'ib_write_bw'
+    elif test == 'wl':
+        testname = 'ib_write_lat'
+    elif test == 'rb':
+        testname = 'ib_read_bw'
+    elif test == 'rl':
+        testname = 'ib_read_lat'
+    else:
+        print('Invalid testing format. (Use one of sb/sl/wb/wl/rb/rl)')
+        exit(-1)
+    return testname
+
+def kill_native_process():
+    bash('echo {} | sudo -S kill -9 $(pgrep ib_)'.format(PASSWD))
+
+      
+def kill_ib():
+    bash('echo comnet02 | sudo kill -9 $(top | pgrep ib_send)')
+    bash('echo comnet02 | sudo kill -9 $(top | pgrep ib_read)')
+    bash('echo comnet02 | sudo kill -9 $(top | pgrep ib_write)')
+
+def async_wait(job):
+    print('Waiting for [{}] to finish'.format(job))
+    FLAG = 1
+    while FLAG:
+        try:
+            processing = bash_return('pgrep ' + job)
+        except:
+            FLAG = 0
+
+############################################
+# Perf test
+############################################
+def perf_test(test, option):
+    """
+    ex. ib_send_bw -S 3 -Q 1 -a
+        -> perf_test('sb', '-S 3 -Q 1 -a')
+    """
+    testname = test_t_translate(test)
+    
+    if '10.' in option:
+        print('Client Wait')
+        time.sleep(0.5)
+    
+    option += ' &'
+    print('[perf_test] {} {}'.format(testname, option))
+    bash('{} {}'.format(testname, option))
+    if '>' in option:
+        res_path = option.split('> ')[1]
+        bash('cat {}'.format(res_path))
+
+if __name__ == '__main__':
+    replace_line('vws_node1', '/freeflow/vws_freeflow/libvws/libvws.h', 57, '#define TRMQ_POLL_TH 5')
+#    restart_router()
+#    restart_vwsshm()
+#    time.sleep(10)
+#    perf_test('vws_node1', 'sb', '-S 3 -Q 1 -a 10.32.0.2')
+#    restart_router()
+#    restart_vwsshm()
+
+#    build_using_sh('vws_node1', '/freeflow/vws_freeflow/libvws/build.sh')
+#    async_wait('vws_node1', 'build.sh')
+
+############################################
+# Perf test
+############################################
+def synchronize(Iam, hostip):
+    HOST = hostip
+    PORT = 8880
+
+    if Iam == 0:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind((HOST, PORT))
+        server_socket.listen()
+    
+        client_socket, addr = server_socket.accept()
+    
+        print('Connected by', addr)
+
+        data = client_socket.recv(10)
+
+        client_socket.sendall('ack'.encode())
+
+        client_socket.close()
+        server_socket.close()
+
+    else:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        while True:
+            try:
+                client_socket.connect((HOST, PORT))
+                break
+            except:
+                time.sleep(0.2)
+        client_socket.sendall('ack'.encode())
+        data = client_socket.recv(10)
+        print('Received', repr(data.decode()))
+
+        client_socket.close()
+
+    print('--------- Sync Completed ------------')
+
+
